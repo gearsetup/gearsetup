bb688ad5770205e2ebdb3cbb40bd4723b42a75ba
Ian Caffey <iancaffey@ymail.com>
2018-08-25 18:08:18 -0500

    Added skill and slot enumerations.

---
+++ geardata/src/main/java/io/gearsetup/data/Skill.java
@@ -0,0 +1,37 @@
+package io.gearsetup.data;
+
+/**
+ * An enumeration of the <a href="http://oldschoolrunescape.wikia.com/wiki/Skills">skills</a> in <a href="https://oldschool.runescape.com/">Old School Runescape</a>.
+ * <p>
+ * Skills are a player's attributes that can be advanced throughout the game. Skills are trained by repetitive actions
+ * that give experience in the skill, until enough experience is earned for the next level. Some skills allow players to
+ * cook food, chop down trees, make fires, use magic and more.
+ *
+ * @author Ian Caffey
+ * @since 1.0
+ */
+public enum Skill {
+    AGILITY,
+    ATTACK,
+    CONSTRUCTION,
+    COOKING,
+    CRAFTING,
+    DEFENCE,
+    FARMING,
+    FIREMAKING,
+    FISHING,
+    FLETCHING,
+    HERBLORE,
+    HITPOINTS,
+    HUNTER,
+    MAGIC,
+    MINING,
+    PRAYER,
+    RANGED,
+    RUNECRAFTING,
+    SLAYER,
+    SMITHING,
+    STRENGTH,
+    THIEVING,
+    WOODCUTTING
+}
---
+++ geardata/src/main/java/io/gearsetup/data/Slot.java
@@ -0,0 +1,24 @@
+package io.gearsetup.data;
+
+/**
+ * An enumeration of the <a href="http://oldschoolrunescape.wikia.com/wiki/Worn_Equipment">Worn Equipment</a> slots in <a href="https://oldschool.runescape.com/">Old School Runescape</a>.
+ * <p>
+ * Equipment is a special type of item that can be equipped in the equipped inventory and will give a character equipment
+ * bonuses. The equipped inventory is divided into 11 slots for each area of the body that some piece of equipment can be worn on.
+ *
+ * @author Ian Caffey
+ * @since 1.0
+ */
+public enum Slot {
+    AMMUNITION,
+    BODY,
+    CAPE,
+    FEET,
+    HANDS,
+    HEAD,
+    LEGS,
+    NECK,
+    RING,
+    SHIELD,
+    WEAPON
+}
