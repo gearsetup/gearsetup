a836e72bdd2c8370dc309b15aacf517c1cbc84b1
Ian Caffey <iancaffey@ymail.com>
2018-08-25 18:32:10 -0500

    Class header documentation formatting fixes.

--- geardata/src/main/java/io/gearsetup/data/AttackType.java
+++ geardata/src/main/java/io/gearsetup/data/AttackType.java
@@ -1,7 +1,8 @@
 package io.gearsetup.data;
 
 /**
- * An enumeration of the <a href="http://oldschoolrunescape.wikia.com/wiki/Attack_types">attack types</a> in <a href="https://oldschool.runescape.com/">Old School Runescape</a>.
+ * An enumeration of the <a href="http://oldschoolrunescape.wikia.com/wiki/Attack_types">attack types</a> in
+ * <a href="https://oldschool.runescape.com/">Old School Runescape</a>.
  * <p>
  * An attack type is a means in which a player or NPC may attack another player, another NPC, or a piece of interactive
  * scenery that can be attacked. Equipment gives players attack bonuses towards and defence bonuses against each of the
--- geardata/src/main/java/io/gearsetup/data/EquipmentSlot.java
+++ geardata/src/main/java/io/gearsetup/data/EquipmentSlot.java
@@ -1,7 +1,8 @@
 package io.gearsetup.data;
 
 /**
- * An enumeration of the <a href="http://oldschoolrunescape.wikia.com/wiki/Worn_Equipment">Worn Equipment</a> slots in <a href="https://oldschool.runescape.com/">Old School Runescape</a>.
+ * An enumeration of the <a href="http://oldschoolrunescape.wikia.com/wiki/Worn_Equipment">Worn Equipment</a> slots in
+ * <a href="https://oldschool.runescape.com/">Old School Runescape</a>.
  * <p>
  * Equipment is a special type of item that can be equipped in the equipped inventory and will give a character equipment
  * bonuses. The equipped inventory is divided into 11 slots for each area of the body that some piece of equipment can be worn on.
