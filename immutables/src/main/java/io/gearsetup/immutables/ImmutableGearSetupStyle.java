ff06fff7fa178882ac959158860d2f52f505444c
Ian Caffey <iancaffey@ymail.com>
2018-08-25 17:39:24 -0500

    Added gearsetup Immutables style.

---
+++ immutables/src/main/java/io/gearsetup/immutables/ImmutableGearSetupStyle.java
@@ -0,0 +1,29 @@
+package io.gearsetup.immutables;
+
+import org.immutables.value.Value.Immutable;
+import org.immutables.value.Value.Style;
+
+/**
+ * An <a href="https://github.com/immutables/immutables">Immutables</a> {@link Style} configuration for the
+ * <a href="https://github.com/gearsetup/gearsetup">gearsetup</a> value classes.
+ * <p>
+ * All <a href="https://github.com/gearsetup/gearsetup">gearsetup</a> value classes are expected to contain the following annotations:
+ * <ul>
+ * <li>{@link Immutable}</li>
+ * <li>{@link ImmutableGearSetupStyle}</li>
+ * </ul>
+ * {@code @Immutable @ImmutableGearSetupStyle public interface GearValue {}}
+ *
+ * @author Ian Caffey
+ * @since 1.0
+ */
+@Style(
+        defaults = @Immutable(prehash = true),
+        get = {"get*", "is*"},
+        init = "set*",
+        allParameters = true,
+        overshadowImplementation = true,
+        depluralize = true
+)
+public @interface ImmutableGearSetupStyle {
+}
