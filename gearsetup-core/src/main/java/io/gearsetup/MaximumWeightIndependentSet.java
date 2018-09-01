20a97a43da62b8542528b2df09da22858ead0384
Ian Caffey <iancaffey@ymail.com>
2018-08-31 22:45:32 -0500

    Reverted to using the vertices and edge predicate directly to allow for more flexibility with delegating to other MWIS implementations for processing sub-graphs.

---
+++ gearsetup-core/src/main/java/io/gearsetup/MaximumWeightIndependentSet.java
@@ -0,0 +1,65 @@
+package io.gearsetup;
+
+import com.google.common.collect.ImmutableSet;
+import lombok.NonNull;
+import lombok.experimental.UtilityClass;
+
+import java.util.Iterator;
+import java.util.Set;
+import java.util.function.BiPredicate;
+import java.util.function.ToDoubleFunction;
+
+/**
+ * A utility class providing an implementation of
+ * <a href="https://en.wikipedia.org/wiki/Independent_set_(graph_theory)#Finding_maximum_independent_sets">maximum-weight independent set</a>
+ * that performs modular decomposition, delegates to {@link RecursiveMaximumWeightIndependentSet} for calculating the
+ * maximum-weight independent set of each <a href="https://en.wikipedia.org/wiki/Connected_component_(graph_theory)">connected component</a>,
+ * and collects the maximum-weight independent subsets into a single set which is then considered maximal for the {@link IndexedGraph}.
+ *
+ * @author Ian Caffey
+ * @since 1.0
+ */
+@UtilityClass
+public class MaximumWeightIndependentSet {
+    /**
+     * Finds the<a href="https://en.wikipedia.org/wiki/Independent_set_(graph_theory)#Finding_maximum_independent_sets">maximum-weight independent set</a>
+     * of the specified graph using the specified weighting function.
+     * <p>
+     * For graphs with {@code IndexedGraph#size() < 2}, {@link IndexedGraph#getVertices()} is returned, as there can only
+     * be a single combination/subset created from the vertices so it is always maximal and graphs with 0 or 1 vertex
+     * cannot have edges.
+     * <p>
+     * For graphs with {@code IndexedGraph#size() >= 2}, the graph is decomposed into its
+     * <a href="https://en.wikipedia.org/wiki/Connected_component_(graph_theory)">connected components</a> and each
+     * component has {@link }
+     *
+     * @param vertices  the vertices of the graph to consider when finding the maximum-weight independent set
+     * @param predicate the edge predicate for determine if two vertices have an edge
+     * @param weight    the weighting function for each candidate
+     * @param <T>       the type of value being used for finding maximum-weight independent set
+     * @return the independent set of vertices that maximize the weight function
+     */
+    public <T> Set<T> find(@NonNull Set<T> vertices, @NonNull BiPredicate<T, T> predicate, ToDoubleFunction<T> weight) {
+        if (vertices.size() < 2) {
+            return ImmutableSet.copyOf(vertices);
+        }
+        ImmutableSet.Builder<T> builder = ImmutableSet.builder();
+        IndexedGraph.of(vertices, predicate).forEachComponent(component -> {
+            //isolated vertex, always maximum and independent
+            if (component.size() < 2) {
+                builder.addAll(component);
+                return;
+            }
+            //two vertex component, choose maximum of the two in the component
+            if (component.size() == 2) {
+                Iterator<T> it = component.iterator();
+                T first = it.next();
+                T second = it.next();
+                builder.add(weight.applyAsDouble(first) >= weight.applyAsDouble(second) ? first : second);
+                return;
+            }
+            builder.addAll(RecursiveMaximumWeightIndependentSet.find(component, predicate, weight));
+        });
+        return builder.build();
+    }
+}
--- gearsetup-core/src/main/java/io/gearsetup/RecursiveMaximumWeightIndependentSet.java
+++ gearsetup-core/src/main/java/io/gearsetup/RecursiveMaximumWeightIndependentSet.java
@@ -14,11 +14,8 @@ import java.util.function.ToDoubleFunction;
  * A utility class providing an implementation of
  * <a href="https://en.wikipedia.org/wiki/Independent_set_(graph_theory)#Finding_maximum_independent_sets">maximum-weight independent set</a>
  * that preforms a recursive, right-to-left, pre-order, pruning <a href="https://en.wikipedia.org/wiki/Depth-first_search">depth-first search</a>
- * over the binary tree of set combinations to find the independent set that maximizes an arbitrary weighting function.
- * <p>
- * {@link RecursiveMaximumWeightIndependentSet} defines a graph as a {@link Set} of vertices with an edge predicate.
- * An edge predicate is a {@link BiPredicate} that takes in two vertices and returns {@code true} if there is an
- * edge between the vertices.
+ * over the binary tree of vertex combinations of a {@link IndexedGraph} to find the independent set that maximizes an arbitrary
+ * weighting function.
  * <p>
  * Each path in the binary tree from the root to the leaf nodes is a combination of the graph vertices, where at each depth
  * {@code d} the node represents the vertex at index {@code d} and each branch of the node can either take or ignore
@@ -39,22 +36,22 @@ import java.util.function.ToDoubleFunction;
 public class RecursiveMaximumWeightIndependentSet {
     /**
      * Finds the <a href="https://en.wikipedia.org/wiki/Independent_set_(graph_theory)#Finding_maximum_independent_sets">maximum-weight independent set</a>
-     * of the specified vertices using the specified edge criteria and weight function.
+     * of the specified graph using the specified weighting function.
      * <p>
-     * For sets with {@code Set#size() < 2}, the specified vertices are returned as an {@link ImmutableSet}, as there can only
+     * For graphs with {@code IndexedGraph#size() < 2}, {@link IndexedGraph#getVertices()} is returned, as there can only
      * be a single combination/subset created from the vertices so it is always maximal and graphs with 0 or 1 vertex
      * cannot have edges.
-     * For sets with {@code Set#size() >= 2}, a recursive, right-to-left, pre-order, pruning
+     * <p>
+     * For sets with {@code IndexedGraph#size() >= 2}, a recursive, right-to-left, pre-order, pruning
      * <a href="https://en.wikipedia.org/wiki/Depth-first_search">depth-first search</a> is performed over the binary tree
-     * of set combinations to find the independent set that maximizes the specified weighting function.
+     * of vertex combinations to find the independent set that maximizes the specified weighting function.
      *
-     * @param vertices the vertices of the graph to consider when finding the maximum-weight independent set
-     * @param criteria the criteria predicate for determining if two vertices have an edge
-     * @param weight   the weighting function for each candidate
-     * @param <T>      the type of vertex being used for finding maximum-weight independent set
+     * @param vertices  the vertices of the graph to consider when finding the maximum-weight independent set
+     * @param predicate the edge predicate for determine if two vertices have an edge
+     * @param <T>       the type of vertex being used for finding maximum-weight independent set
      * @return the set of candidates that maximize the weight function without being adjacent to other members of the set
      */
-    public <T> Set<T> find(@NonNull Set<T> vertices, @NonNull BiPredicate<T, T> criteria, @NonNull ToDoubleFunction<T> weight) {
+    public <T> Set<T> find(@NonNull Set<T> vertices, @NonNull BiPredicate<T, T> predicate, @NonNull ToDoubleFunction<T> weight) {
         if (vertices.size() < 2) {
             return ImmutableSet.copyOf(vertices);
         }
@@ -65,7 +62,7 @@ public class RecursiveMaximumWeightIndependentSet {
         int[] maximum = new int[size];
         double[] maximumWeight = new double[1];
         int[] maximumSize = new int[1];
-        fill(values, criteria, weight, selected, 0, 0, maximum, maximumSize, maximumWeight);
+        fill(values, predicate, weight, selected, 0, 0, maximum, maximumSize, maximumWeight);
         //maximum[0, maximumSize[0]] is the maximum-weight independent set
         ImmutableSet.Builder<T> builder = ImmutableSet.builder();
         for (int i = 0; i < maximumSize[0]; i++) {
@@ -79,7 +76,7 @@ public class RecursiveMaximumWeightIndependentSet {
      * over the binary tree of set combinations to find the independent set that maximizes an arbitrary weighting function.
      *
      * @param vertices      the vertices of the graph to consider when finding the maximum-weight independent set
-     * @param criteria      the criteria predicate for determining if two vertices have an edge
+     * @param predicate     the edge predicate for determine if two vertices have an edge
      * @param weight        the weighting function for each vertex
      * @param selected      the buffer for the currently selected vertices in the graph
      * @param selectedCount the length of the sub-array in the selected buffer
@@ -89,7 +86,7 @@ public class RecursiveMaximumWeightIndependentSet {
      * @param maximumWeight the buffer to hold the weight of the maximum-weight independent set
      * @param <T>           the type of value being used for finding maximum-weight independent set
      */
-    private <T> void fill(@NonNull List<T> vertices, @NonNull BiPredicate<T, T> criteria, @NonNull ToDoubleFunction<T> weight,
+    private <T> void fill(@NonNull List<T> vertices, @NonNull BiPredicate<T, T> predicate, @NonNull ToDoubleFunction<T> weight,
                           int[] selected, int selectedCount, int depth,
                           @NonNull int[] maximum, @NonNull int[] maximumSize, @NonNull double[] maximumWeight) {
         //traverse tree until recursive call reaches leaf node
@@ -97,7 +94,7 @@ public class RecursiveMaximumWeightIndependentSet {
             boolean independent = true;
             T current = vertices.get(depth);
             for (int i = 0; i < selectedCount; i++) {
-                if (criteria.test(current, vertices.get(selected[i]))) {
+                if (predicate.test(current, vertices.get(selected[i]))) {
                     independent = false;
                     break;
                 }
@@ -105,12 +102,12 @@ public class RecursiveMaximumWeightIndependentSet {
             if (independent) {
                 selected[selectedCount++] = depth;
                 //continue looking for a maximum independent set which contains the current vertex
-                fill(vertices, criteria, weight, selected, selectedCount, depth + 1, maximum, maximumSize, maximumWeight);
+                fill(vertices, predicate, weight, selected, selectedCount, depth + 1, maximum, maximumSize, maximumWeight);
                 //discard current vertex from selection, to allow finding maximum independent sets that do not contain the current vertex
                 --selectedCount;
             }
             //continue looking for a maximum independent set, skipping the vertex at the current depth as it intersected with the current selection
-            fill(vertices, criteria, weight, selected, selectedCount, depth + 1, maximum, maximumSize, maximumWeight);
+            fill(vertices, predicate, weight, selected, selectedCount, depth + 1, maximum, maximumSize, maximumWeight);
             return;
         }
         //reached leaf node for path, accumulate weight of independent vertices
